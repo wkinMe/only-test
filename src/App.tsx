2fe107f013ecbf7aa94f4541dec6af5144e23b80
name <you@example.com>
2025-03-31 11:07:07 +0300

    layout: Main block initial layout has been created

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Dates from './components/Dates'
 
 function App() {
-  
+
   return (
+    <Dates />
   )
 }
 
--- src/components/Dates/index.tsx
+++ src/components/Dates/index.tsx
@@ -1,15 +1,38 @@
 import { useState } from "react"
 import styles from "./style.module.scss"
+import { clsx } from "clsx";
 
-export default function Home() {
-    const [startDate, setStartDate] = useState();
-    const [endDate, setEndDate] = useState();
+export default function Dates() {
+    const [startDate, setStartDate] = useState(2015);
+    const [endDate, setEndDate] = useState(2022);
+    const [datesCount, setDatesCount] = useState(6);
+    const [currentDate, setCurrentDate] = useState(1);
+
+    const prevButtonClass = clsx(styles.prevButton, currentDate == 1 && styles.disabled)
+    const nextButtonClass = clsx(styles.nextButton, datesCount == currentDate && styles.disabled)
+
+    const handlePrevClick = () => {
+        if (currentDate > 1) {
+            setCurrentDate((prev) => prev - 1);
+        }
+    }
+
+    const handleNextClick = () => {
+        if (currentDate < datesCount) {
+            setCurrentDate((prev) => prev + 1);
+        }
+    }
     
     return <div className={styles.datesContainer}>
-        <h1>Исторические даты</h1>
-        <div className={styles.circle}></div>
-        <h2>{startDate}</h2>
-        <h2>{endDate}</h2>
-
+        <h2>Исторические даты</h2>
+        <div className={styles.content}>
+            <div className={styles.circle}></div>
+            <h1><span className={styles.dateStart}>{startDate}</span> <span className={styles.dateEnd}>{endDate}</span></h1>
+        </div>
+        <div className={styles.datesPagination}>
+            <span className={styles.datesCounter}>0{currentDate}/0{datesCount}</span>
+            <button className={prevButtonClass} onClick={handlePrevClick}>{"<"}</button>
+            <button className={nextButtonClass} onClick={handleNextClick}>{">"}</button>
+        </div>
     </div>
 }
