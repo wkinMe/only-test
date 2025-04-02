b8cc52baab54ed68aacbaa2667b6b6181522a5d3
name <you@example.com>
2025-04-02 11:54:37 +0300

    fix: DatesText animation behavior

--- src/components/DatesText/index.tsx
+++ src/components/DatesText/index.tsx
@@ -3,7 +3,6 @@ import { gsap } from 'gsap';
 import styles from './styles.module.scss';
 import { CIRCLE_ROTATION_TIME, EASE_FUNCTION } from '@constants/constants';
 
-
 interface DatesTextProps {
     startDate: number;
     endDate: number;
@@ -13,17 +12,21 @@ export default function DatesText({ startDate, endDate }: DatesTextProps) {
     const startDateRef = useRef<HTMLSpanElement | null>(null);
     const endDateRef = useRef<HTMLSpanElement | null>(null);
 
-    const [displayedStartDate, setDisplayedStartDate] = useState<number>(0);
-    const [displayedEndDate, setDisplayedEndDate] = useState<number>(0);
+    const [displayedStartDate, setDisplayedStartDate] = useState<number>(startDate);
+    const [displayedEndDate, setDisplayedEndDate] = useState<number>(endDate);
+
+    // Флаг для отслеживания первого рендера
+    const isInitialRender = useRef(true);
 
     useEffect(() => {
-        setDisplayedStartDate(startDate);
-        setDisplayedEndDate(endDate);
+        if (isInitialRender.current) {
+            // Устанавливаем флаг в false после первого рендера
+            isInitialRender.current = false;
+            return;
+        }
 
         const animateNumber = (element: HTMLSpanElement, newValue: number) => {
-            const currentValue = parseInt(element.textContent || '0', 10);
-            const duration = CIRCLE_ROTATION_TIME;
-            const ease = EASE_FUNCTION;
+            const currentValue = parseInt(element.textContent || '0');
 
             if (currentValue !== newValue) {
                 gsap.fromTo(
@@ -31,14 +34,9 @@ export default function DatesText({ startDate, endDate }: DatesTextProps) {
                     { textContent: currentValue },
                     {
                         textContent: newValue,
-                        duration,
-                        ease,
                         snap: { textContent: 1 },
-                        onUpdate: () => {
-                            element.textContent = Math.round(
-                                parseFloat(element.textContent || '0'),
-                            ).toString();
-                        },
+                        duration: CIRCLE_ROTATION_TIME,
+                        ease: EASE_FUNCTION,
                     },
                 );
             }
